498c082ba4ed9e0aa95c40474f2bf5cdc3ae8f89
kelvinkauan <kelvinkauan9@gmail.com>
2023-04-13 12:00:19 -0300

    ....

--- PaginaPrincipal/PaginaPrincipal.php
+++ PaginaPrincipal/PaginaPrincipal.php
@@ -1,13 +1,65 @@
 <!DOCTYPE html>
 <html>
-    
-<head>
 
+<head>
+    <meta charset="utf-8">
+    <title> SESMT Pagina Principal</title>
+    <link rel="stylesheet" href="./Style/PaginaPrincipal.css">
+ 
 </head>
 
 
 <body>
-    
+<nav>
+<div class="img">
+        <img src="../imagens/logBranca_SESMT.png"alt="">
+    </div>
+   <ul class="sub-menu">
+    <a>
+           <li>Cadastro</li>
+       </a>
+       <a href="#">
+           <li>Formulários
+           <ul >
+               <li><a href="#">Relato de Perigo</a></li>
+               <li><a href="#">APR Gravada</a></li>
+               <li><a href="#">Check List Veículo</a></li>
+               <li><a href="#">Check List EPI</a></li>
+               <li><a href="#">Equipamentos</a></li>
+               <li><a href="#">5RO</a></li>
+               <li><a href="#">Foto Ponto</a></li>
+               <li><a href="#">Ensaios</a></li>
+               <li><a href="#">Consulta Equipe</a></li>
+           </ul>
+
+           </li>
+       </a>
+       <a href="#">
+           <li>Inspeção</li>
+       </a>
+       <a href="#">
+           <li>Preleção</li>
+       </a>
+       <a href="#">
+           <li>Colaborador</li>
+       </a>
+       <a href="#">
+           <li>TG/TF</li>
+       </a>
+       <a href="#">
+           <li>Frota</li>
+       </a>
+       <a href="#">
+           <li>Power BI</li>
+       </a>
+       <a href="#">
+           <li>Relatório</li>
+       </a>
+       
+
+   </ul>
+
+</nav>
 
 </body>
 
